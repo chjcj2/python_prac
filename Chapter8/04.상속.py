0a987bafb2754d7a168707aef77c2a0f126e9633
chj <chjcj2@naver.com>
2024-08-18 14:21:39 +0900

    feat : 상속,오버라이딩,클래스변수

---
+++ Chapter8/04.상속.py
@@ -0,0 +1,31 @@
+# 상속 
+# : 클래스들에 중복된 코드를 제거하고 유지보수를
+#   편하게 하기 위해서 사용.
+
+# 부모 클래스
+class Monster:
+    def __init__(self, name, health, attack):   # __init__ 메서드는 클래스의 속성을 정의할때 쓴다?(hj)
+        self.name = name
+        self.health = health
+        self.attack = attack
+    def move(self):
+        print(f"[{self.name}] 지상에서 이동하기")
+
+# 자식 클래스
+class Wolf(Monster):
+    pass
+
+class Shark(Monster):
+    def move(self): # 메서드 오버라이딩
+        print(f"[{self.name}] 헤엄치기")
+
+class Dragon(Monster):
+    def move(self): # 메서드 오버라이딩
+        print(f"[{self.name}] 날기")
+
+wolf = Wolf("울프", 1500, 200)
+wolf.move()
+shark = Shark("샤크", 3000, 400)
+shark.move()
+dragon = Dragon("드래곤", 8000, 800)
+dragon.move()
