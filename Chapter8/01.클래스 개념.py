0bdd4a993efdcfcf60e4542ae247bdbd6c178ed2
chj <chjcj2@naver.com>
2024-08-18 13:16:35 +0900

    feat : 챔피언 하나 더 추가(야스오)

--- Chapter8/01.클래스 개념.py
+++ Chapter8/01.클래스 개념.py
@@ -3,7 +3,7 @@
 
 # 클래스를 사용하는 이유
 
-print("=========== 클래스를 사용하지 않은 경우 ===========12줄로 구현, 그러나 100여개의 챔피언을 다 구현한다면?")
+print("=========== 클래스를 사용하지 않은 경우 ===========17줄로 구현, 그러나 100여개의 챔피언을 다 구현한다면?")
 
 champion1_name = "이즈리얼"
 champion1_health = 700
@@ -17,13 +17,20 @@ champion2_attack = 95
 
 print(f"{champion2_name}님 소환사의 협곡에 오신것을 환영합니다.")
 
+champion3_name = "야스오"
+champion3_health = 750
+champion3_attack = 92
+
+print(f"{champion3_name}님 소환사의 협곡에 오신것을 환영합니다.")
+
 def basic_attack(name, attack):
     print(f"{name} 기본공격 {attack}")
 
 basic_attack(champion1_name, champion1_attack)
 basic_attack(champion2_name, champion2_attack)
+basic_attack(champion3_name, champion3_attack)
 
-print("=========== 클래스를 사용한경우 ===========12줄로 구현 가능, 챔피언이 많아질수록 구현이 편하다")
+print("=========== 클래스를 사용한경우 ===========14줄로 구현, 챔피언이 많아질수록 편해진다")
 
 class Champion:
     def __init__(self, name, health, attack):
@@ -36,7 +43,9 @@ class Champion:
 
 ezreal = Champion("이즈리얼", 700, 90)
 leesin = Champion("리신", 800, 95)
+yasuo = Champion("야스오", 700, 92)
 ezreal.basic_attack()
 leesin.basic_attack()
+yasuo.basic_attack()
 
     
