046539e70e067a33518e489824f2be92e9f7a493
chj <chjcj2@naver.com>
2024-08-31 20:46:23 +0900

    feat : 실습프로젝트 코드 일부수정

--- Chapter12/main.py
+++ Chapter12/main.py
@@ -4,7 +4,8 @@ import csv
 from post import Post
 
 # 파일경로
-file_path = "./Chapter12/data.csv"  #현재는 "python_prac" 이 기본경로여서 그 이하로 경로를 적어주었다
+file_path = "./python_prac/Chapter12/data.csv"  
+# 현재는 "python_prac" 이 기본경로인지 xxx_git_code가 기본경로인지에 따라 다르다
 
 # post 객체를 저장할 리스트
 post_list = []
@@ -20,7 +21,6 @@ if os.path.exists(file_path):
         # Post 인스턴스 생성하기
         post = Post(int(data[0]),data[1],data[2],int(data[3]))
         post_list.append(post)
-    pass
 else:
     # 파일 생성하기
     f = open(file_path, "w", encoding="utf8", newline="")
